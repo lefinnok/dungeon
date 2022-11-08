0131a06fcc073d6db848d254956b4b2fd5fb875d
Lefinno KWOK <lefinnokwok@gmail.com>
2022-11-08 13:30:03 +0800

    modify ignore

--- dev/saveload.cpp
+++ dev/saveload.cpp
@@ -4,6 +4,7 @@
 #else
 #include <unistd.h>
 #define GetCurrentDir getcwd
+//#include "libs/llvm-15.0.2.src/BinaryFormat/ELF.h"
 #endif
 
 #include <iostream>
@@ -16,6 +17,8 @@ using namespace std;
 //https://www.tutorialspoint.com/find-out-the-current-working-directory-in-c-cplusplus
 //https://blog.csdn.net/wteruiycbqqvwt/article/details/112664432
 //https://stackoverflow.com/questions/143174/how-do-i-get-the-directory-that-a-program-is-running-from
+//Dynamic Function Load [Name]:
+//https://stackoverflow.com/questions/25270275/get-functions-names-in-a-shared-library-programmatically
 
 
 int get_current_dir(char pBuf[FILENAME_MAX]) {
@@ -34,7 +37,58 @@ int get_current_dir(char pBuf[FILENAME_MAX]) {
 
 
 namespace dg{
-    
+    /*
+    static int callback(struct dl_phdr_info *info, size_t size, void *data)
+    {
+        // data is copy of 2nd arg in dl_iterate_phdr
+        // you can use it for your lib name as I did
+        const char * libname = (const char *)data;
+
+        // if current elf's name contains your lib
+        if (strstr(info->dlpi_name, libname))
+        {
+
+            printf("loaded %s from: %s\n", libname, info->dlpi_name);
+
+            for (int j = 0; j < info->dlpi_phnum; j++)
+            {
+                // we need to save dyanmic section since it contains symbolic table
+                if (info->dlpi_phdr[j].p_type == PT_DYNAMIC)
+                {
+                    Elf64_Sym * symtab = nullptr;
+                    char * strtab = nullptr;
+                    int symentries = 0;
+                    auto dyn = (Elf64_Dyn *)(info->dlpi_addr + info->dlpi_phdr[j].p_vaddr);
+                    for (int k = 0; k < info->dlpi_phdr[j].p_memsz / sizeof(Elf64_Dyn); ++k)
+                    {
+                        if (dyn[k].d_tag == DT_SYMTAB)
+                        {
+                            symtab = (Elf64_Sym *)dyn[k].d_un.d_ptr;
+                        }
+                        if (dyn[k].d_tag == DT_STRTAB)
+                        {
+                            strtab = (char*)dyn[k].d_un.d_ptr;
+                        }
+                        if (dyn[k].d_tag == DT_SYMENT)
+                        {
+                            symentries = dyn[k].d_un.d_val;
+                        }
+                    }
+                    int size = strtab - (char *)symtab;
+                    // for each string in table
+                    for (int k = 0; k < size / symentries; ++k)
+                    {
+                        auto sym = &symtab[k];
+                        auto str = &strtab[sym->st_name];
+                        printf("%s\n", str);
+                    }
+                    break;
+                }
+            }
+        }
+        return 0;
+    }
+    */
     int loadEvents(){
         xml_document eventDB;
         char pBuf[FILENAME_MAX];
