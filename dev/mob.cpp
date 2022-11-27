e7bac883df9a5944d8610d2e1cc7580594bb969d
Lefinno KWOK <lefinnokwok@gmail.com>
2022-11-27 22:25:29 +0800

    mob scafolded

--- dev/mob.cpp
+++ dev/mob.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
-#include "mob.h"
+#include "mob.hpp"
+using namespace std;
+
+namespace dg{
+	map<int,int> modifier_convert={
+		{1,-3},
+		{2,-3},
+		{20,3}
+	};
+}
 //get modifier
---
+++ dev/mob.hpp
@@ -0,0 +1,64 @@
+#ifndef _MOB_H_
+#define _MOB_H_
+#include <map>
+#include <vector>
+#include <string>
+namespace dg{
+	//references and inspiration from mork borg
+	extern std::map<int,int> modifier_convert;
+	class mob;
+	class attributes{
+		public:
+			//constructor
+			attributes();
+			int agility;
+			int presence;
+			int strength;
+			int toughness;
+			//get modifer from attribute
+			int getAgilityMod();
+			int getPresenceMod();
+			int getStrengthMod();
+			int getToughnessMod();	
+	};
+	//can implement later if have time
+	class effect{
+		
+	};
+	class equipment{
+		public:
+			//constructor
+			equipment();
+			//called when the equipment is used against a target
+			virtual void use(mob* target);
+	};
+	class melee_weapon:equipment{
+		public:
+			//constructor
+			melee_weapon();
+			void use(mob* target, attributes* user_attributes);
+	};
+	//implement later
+	class appendage{
+		public:
+			//constructor
+			appendage();
+			equipment eq;
+					
+	};
+
+	class mob{
+		public:
+			//constructor
+			mob();
+			//deconstructor
+			~mob();
+		private:
+			int base_hp;
+			int base_ac;
+			attributes attr;
+			std::string armor;
+			std::string weapon;
+	};
+}
+#endif
