96ad2eadffbc8843db48d41c8220efb19b90aeaf
xfdxfdxfd <110054734+xfdxfdxfd@users.noreply.github.com>
2022-11-27 22:57:00 +0800

    Update mob.cpp

--- dev/mob.cpp
+++ dev/mob.cpp
@@ -8,7 +8,12 @@ using namespace std;
 namespace dg{
 	// modifier for converting abilities to the chance of success rate.
 	// If the ability is 1-4, then the success rate of that ability decrease by 3
-	// If the ability is 5-6, then the success rate of that ability decrease by 3
+	// If the ability is 5-6, then the success rate of that ability decrease by 2
+	// If the ability is 7-8, then the success rate of that ability decrease by 1
+	// If the ability is 9-12, then the success rate of that ability remains no change
+	// If the ability is 13-14, then the success rate of that ability increase by 1
+	// If the ability is 15-16, then the success rate of that ability increase by 2
+	// If the ability is 17-20, then the success rate of that ability increase by 3
 	map<int,int> modifier_convert={
 		{1,-3},
 		{2,-3},
@@ -22,14 +27,13 @@ namespace dg{
 		{10,0},
 		{11,0},
 		{12,0},
-		{13,-3},
-		{14,-3},
-		{15,-3},
-		{16,-3},
-		{17,-3},
-		{18,-3},
-		{19,-3},
-		{20,-3},
+		{13,1},
+		{14,1},
+		{15,2},
+		{16,2},
+		{17,3},
+		{18,3},
+		{19,3},
 		{20,3}
 	};
 	mob::mob(){
