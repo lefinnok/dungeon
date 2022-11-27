cc399da431055eba435b263feffa8d8addfa7927
xfdxfdxfd <110054734+xfdxfdxfd@users.noreply.github.com>
2022-11-27 22:24:31 +0800

    Update mob.cpp

--- dev/mob.cpp
+++ dev/mob.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 #include "mob.h"
+#include <map>
+#include <string>
+
+using namespace std;
+
+class mobs{
+  public:
+    int base_health;
+    int base_strength;
+  
+}
+int main(){
+  
+  map<int,int> AGILITY;
+  map<int,int> PRESENCE;
+  map<int,int> STRENGTH;
+  map<int,int> TOUGHNESS;
+    
+}
 //get modifier
